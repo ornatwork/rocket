b6f0ed8f8ea7c881624dc83b748bac38b8ee7b08
orn.kristjansson <okristjansson@deloitte.com>
2014-11-04 12:11:52 -0700

    Broke out enums

---
+++ src/us/kristjansson/rocket/Authoration_State.java
@@ -0,0 +1,10 @@
+//
+package us.kristjansson.rocket;
+
+public enum Authoration_State 
+{
+	START,
+	USER, 
+	PASS, 
+	LOGGED_IN 
+}
---
+++ src/us/kristjansson/rocket/Transaction_State.java
@@ -0,0 +1,9 @@
+//
+package us.kristjansson.rocket;
+
+public enum Transaction_State 
+{
+	START, 
+	READY_FIRE, 
+	FIRE 
+}
