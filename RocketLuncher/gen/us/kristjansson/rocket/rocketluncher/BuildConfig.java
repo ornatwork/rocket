64e5a215896e90ad23de352661da989fbd121345
orn.kristjansson <okristjansson@deloitte.com>
2014-11-05 17:49:09 -0700

    Android rocket luncher client, initial

---
+++ RocketLuncher/gen/us/kristjansson/rocket/rocketluncher/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package us.kristjansson.rocket.rocketluncher;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ RocketLuncher/src/us/kristjansson/rocket/rocketluncher/CxLogger.java
@@ -0,0 +1,7 @@
+//
+package us.kristjansson.rocket.rocketluncher;
+
+public class CxLogger 
+{
+
+}
