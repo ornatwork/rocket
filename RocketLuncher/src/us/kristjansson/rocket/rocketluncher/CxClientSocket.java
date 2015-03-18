b0c8b76fc55b034dbab390562d33b8ccd94eac1e
orn.kristjansson <okristjansson@deloitte.com>
2015-03-18 14:05:40 -0600

    Fixed spelling error

--- RocketLuncher/src/us/kristjansson/rocket/rocketluncher/CxClientSocket.java
+++ RocketLuncher/src/us/kristjansson/rocket/rocketluncher/CxClientSocket.java
@@ -1,5 +1,6 @@
 //
 package us.kristjansson.rocket.rocketluncher;
+
 //
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -15,13 +16,12 @@ import android.content.Intent;
 // Sample service 
 // http://developer.android.com/guide/components/bound-services.html#Binder
 
-public class CxClientSocket 
-{
-	// IP and port 
-	//private static final String HOST = "192.168.1.10";
-	private static final String HOST = "10.17.181.101";
-	//private static final String HOST = "localhost";
-	//private static final String HOST = "127.0.0.1";
+public class CxClientSocket {
+	// IP and port
+	// private static final String HOST = "192.168.1.10";
+	private static final String HOST = "192.168.1.10";
+	// private static final String HOST = "localhost";
+	// private static final String HOST = "127.0.0.1";
 	private static final int PORT_NUMBER = 7007;
 	//
 	private static Socket clientSocket = null;
@@ -33,121 +33,95 @@ public class CxClientSocket
 	// Notifier
 	private OnMessageReceived mMessageListener = null;
 	private boolean mbRun = true;
-	
 
-	
-	public CxClientSocket(OnMessageReceived listener) 
-    {
-        mMessageListener = listener;
-    }
+	public CxClientSocket(OnMessageReceived listener) {
+		mMessageListener = listener;
+	}
 
-     public void run() 
-	 {
-    	 
-    	 CxLogger.i("run...");
-    	 msResponse = this.connect();
-    	 if( msResponse  != null && mMessageListener != null) 
-    	 {
-             // call the method messageReceived from MyActivity class
-             mMessageListener.messageReceived( msResponse );
-         }
-    	 
-    	 
-    	 // Keep it going until run is not true anymore  
-    	 while( mbRun )
-    	 {
-    		 try
-    		 {
-	    		 if( msResponse != null && mMessageListener != null) 
-	        	 {
-	    			if( msCommand.length() > 0 )
-	    			{
-	    				outToServer.writeBytes( msCommand + '\n');
-	    				// 	Read server response
-	    				msResponse = inFromServer.readLine();
-	    				CxLogger.e("from server=" + msResponse );
-	    				mMessageListener.messageReceived( msResponse );
-	    				
-	    				
-	    				if( msCommand.equals("QUIT"))
-		   	    		{
-		   	    			 mbRun = false;
-		   	    			 this.Close();
-		   	    		}
-		   	    		// wipe clean
-		   	    		msCommand = "";
-	    			}
-	        	 }
-	    		 
-	    		 
-    		 }
-    		 catch( Exception ex )
-    		 {
-    			 CxLogger.e("in run", ex );
-    		 }
-    			 
-    	 }
+	public void run() {
 
-		 CxLogger.i("run is done ");
-	 }
- 
-     
-	// setup 
-    private String connect()
-    {
-		 //
-		 String response = "";
-		 //
-		 CxLogger.i( "Connecting to Rocket server" );
-		 
-		try
-		{
-			 clientSocket = new Socket( HOST, PORT_NUMBER );
-			 outToServer = new DataOutputStream(clientSocket.getOutputStream());
-			 
-			 // Connect and interact with the server
-			 inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			 // Read the greeting first
-			 response = inFromServer.readLine();
-			 CxLogger.i( response );
-			 
+		CxLogger.i("run...");
+		msResponse = this.connect();
+		if (msResponse != null && mMessageListener != null) {
+			// call the method messageReceived from MyActivity class
+			mMessageListener.messageReceived(msResponse);
+		}
+
+		// Keep it going until run is not true anymore
+		while (mbRun) {
+			try {
+				if (msResponse != null && mMessageListener != null) {
+					if (msCommand.length() > 0) {
+						outToServer.writeBytes(msCommand + '\n');
+						// Read server response
+						msResponse = inFromServer.readLine();
+						CxLogger.e("from server=" + msResponse);
+						mMessageListener.messageReceived(msResponse);
+
+						if (msCommand.equals("QUIT")) {
+							mbRun = false;
+							this.Close();
+						}
+						// wipe clean
+						msCommand = "";
+					}
+				}
+
+			} catch (Exception ex) {
+				CxLogger.e("in run", ex);
+			}
+
+		}
+
+		CxLogger.i("run is done ");
+	}
+
+	// setup
+	private String connect() {
+		//
+		String response = "";
+		//
+		CxLogger.i("Connecting to Rocket server");
+
+		try {
+			clientSocket = new Socket(HOST, PORT_NUMBER);
+			outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+			// Connect and interact with the server
+			inFromServer = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
+			// Read the greeting first
+			response = inFromServer.readLine();
+			CxLogger.i(response);
+
 			//
 			return response;
-		}
-		catch( Exception Ex )
-		{
+		} catch (Exception Ex) {
 			CxLogger.e("Connection error", Ex);
 			return "Error=" + Ex;
 		}
-    }
-    
-    // Send command to the server 
-    public void sendCommand( String psCommand )
-    {
+	}
+
+	// Send command to the server
+	public void sendCommand(String psCommand) {
 		//
-		CxLogger.i( "send command to Rocket server=" + psCommand );
+		CxLogger.i("send command to Rocket server=" + psCommand);
 		this.msCommand = psCommand;
-    }
+	}
 
-   
-    public void Close() 
-    {
-		try 
-		{
-			//  Bye 
-			if( clientSocket != null && clientSocket.isConnected() )
+	public void Close() {
+		try {
+			// Bye
+			if (clientSocket != null && clientSocket.isConnected())
 				clientSocket.close();
-		} 
-		catch (IOException Ex ) 
-		{
-			CxLogger.e("Connection error", Ex );
+		} catch (IOException Ex) {
+			CxLogger.e("Connection error", Ex);
 		}
-    }
+	}
 
-    // Send message back
-    public interface OnMessageReceived 
-    {
-        public void messageReceived(String message);
-    }
+	// Send message back
+	public interface OnMessageReceived {
+		public void messageReceived(String message);
+	}
 
-}  // EOC
+} // EOC
